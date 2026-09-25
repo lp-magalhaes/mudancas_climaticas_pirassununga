cbbe8639590200ceda0e1bf5ff6ba05e2561e03b
Leonardo Pinto de Magalhães <141336974+lp-magalhaes@users.noreply.github.com>
2026-09-25 18:10:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ col2.metric("📉 Alteração Chuva (IPCC)", f"{delta_chuva_percentual:.1f} %")
 v_sim_ago = float(df_sim['Vazao_Sim'].iloc[10])
 v_base_ago = float(df_sim['Vazao_Base'].iloc[10])
 queda_vazao_ago = ((v_sim_ago - v_base_ago) / v_base_ago) * 100
-col3.metric("🚨 Vazão Novembro (menor vazão)", f"{queda_vazao_ago:.1f} %")
+col3.metric("🚨 Redução menor vazão", f"{queda_vazao_ago:.1f} %")
 
 pico_custo_mensal = max(custos_incremento_mensal)
 col4.metric("💰 Pico de Custo Químico", f"+{pico_custo_mensal:.2f} %")
