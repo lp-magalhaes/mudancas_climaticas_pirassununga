b5fb26cbf22073945891bfaf50a5c98e553d150c
Leonardo Pinto de Magalhães <141336974+lp-magalhaes@users.noreply.github.com>
2026-09-25 18:32:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -86,15 +86,15 @@ c_sim = list(df_sim['Chuva_Sim'].values)
 
 for i in range(12):
     # ant assume 11 (Dezembro) quando i é 0 (Janeiro)
-    ant = (i - 2) % 12       
+    ant = (i - 1) % 12       
     
     # Regra de 45 dias: Mês atual (30 dias) + metade do mês anterior (15 dias)
-    chuva_45_base.append(c_base[i-1] + (c_base[ant] * 0.5))
-    chuva_45_sim.append(c_sim[i-1] + (c_sim[ant] * 0.5))
+    chuva_45_base.append(c_base[i] + (c_base[ant] * 0.5))
+    chuva_45_sim.append(c_sim[i] + (c_sim[ant] * 0.5))
     
     # Regra de 60 dias: Mês atual (30 dias) + mês anterior completo (30 dias)
-    chuva_60_base.append(c_base[i-1] + c_base[ant])
-    chuva_60_sim.append(c_sim[i-1] + c_sim[ant])
+    chuva_60_base.append(c_base[i] + c_base[ant])
+    chuva_60_sim.append(c_sim[i] + c_sim[ant])
 
 df_sim['Chuva_45_Base'] = chuva_45_base
 df_sim['Chuva_45_Sim'] = chuva_45_sim
